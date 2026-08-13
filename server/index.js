1d76234c8c354af43fc7564d08682026e2535dc1
TanishChhalotre <tanishchhalotreemail@gmail.com>
2026-08-13 22:45:03 +0530

    Fix trust proxy for Render

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const { connectDB }    = require('./db');
 const { seedDatabase } = require('./seed');
 
 const app  = express();
+
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:5173'].filter(Boolean);
 
